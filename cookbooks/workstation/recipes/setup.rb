e0469516a2607b4b7a5b5dde97c396872ee68732
Gerard Recinto <gerardrecinto@gmail.com>
2021-04-28 06:01:40 +0000

    checkpoint_with_variablerefs

--- cookbooks/workstation/recipes/setup.rb
+++ cookbooks/workstation/recipes/setup.rb
@@ -1,4 +1,5 @@
 package 'vim-enhanced'
+package 'emacs'
 package 'nano'
 
 package 'tree' do
@@ -9,10 +10,29 @@ package 'git' do
   action :install
 end
 
+=begin
+script 'host' do
+  interpreter "bash"
+  cwd ::File.dirname(src_filepath)
+  code <<-EOH
+    echo "Hostname: " $(hostname)
+  EOH
+end
+=end
+
+node['ipaddress']
+node['memory']['total']
+
 package 'ntp'
 file '/etc/motd' do
-  content 'testing'
+  content "
+  HOSTNAME: #{node['hostname']}
+  IPADDRESS: #{node['ipaddress']}
+  CPU: #{node['cpu']['0']['mhz']}
+  MEMORY: #{node['memory']['total']}
+"
   action :create
   owner 'root'
   group 'root'
 end
+
